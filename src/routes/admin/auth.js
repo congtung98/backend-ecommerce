981e912bc098a768c86f6cfbdf6006680a11e36a
trancongtung <congtunggv@gmail.com>
2020-12-31 10:47:18 +0700

    create signout route

--- src/routes/admin/auth.js
+++ src/routes/admin/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/admin/signup', validateSignupRequest, isRequestValidated, signUp);
 router.post('/admin/signin', validateSigninRequest, isRequestValidated, signIn);
-router.post('/admin/signout', requireSignIn, signOut);
+router.post('/admin/signout', signOut);
 
 // router.post('/profile', requireSignIn, (req, res) => {
 //     res.status(200).json({ user: 'profile' })
