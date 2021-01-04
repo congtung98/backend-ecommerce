33a24a510aa115d5c8f5fad2e8ca18fd95b72c32
trancongtung <congtunggv@gmail.com>
2021-01-04 08:56:45 +0700

    fix token expired

--- src/controller/admin/auth.js
+++ src/controller/admin/auth.js
@@ -45,9 +45,9 @@ exports.signIn = (req, res) => {
         if(error) return res.status(400).json({ error });
         if(user){
             if(user.authenticate(req.body.password) && user.role === 'admin'){
-                const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
+                const token = jwt.sign({ _id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '24h' });
                 const { _id, firstName, lastName, email, role, fullName } = user;
-                res.cookie('token', token, { expiresIn: '1h' });
+                res.cookie('token', token, { expiresIn: '24h' });
                 res.status(200).json({
                     token,
                     user: {
