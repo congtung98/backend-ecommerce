904596e6b8bf019a8065f54ddddf731e495a376e
Hoan Vu Van <congtunggv@gmail.com>
2021-01-20 09:01:05 +0700

    update code bcrypt to async

--- src/controller/admin/initialData.js
+++ src/controller/admin/initialData.js
@@ -28,9 +28,9 @@ function createCategories(categories, parentId = null){
 exports.initialData = async (req, res) => {   
     const categories = await Category.find({}).exec();
     const products = await Product.find({})
-                                    .select('_id name price quantity slug description productPictures category')
-                                    // .populate('category')
-                                    .exec();
+        .select('_id name price quantity slug description productPictures category')
+        .populate({ path: 'category', select: '_id name' })
+        .exec();
     res.status(200).json({
         categories: createCategories(categories),
         products
