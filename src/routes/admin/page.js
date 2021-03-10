d599735f6709713ec4886482c7961bab094453b5
Hoan Vu Van <congtunggv@gmail.com>
2021-03-10 10:46:37 +0700

    update page product backend

--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPage } = require('../../controller/admin/page');
+const { createPage, getPage } = require('../../controller/admin/page');
 const { upload, requireSignIn, adminMiddleware } = require('../../middleware');
 const router = express.Router();
 
@@ -7,5 +7,7 @@ router.post(`/page/create`, requireSignIn, adminMiddleware, upload.fields([
     { name: 'banners' },
     { name: 'products' }
 ]), createPage)
+
+router.get(`/page/:category/:type`, getPage);
 
 module.exports = router;
