185fbf53792d58b1916e55e5e4726d2876b75d81
Hoan Vu Van <congtunggv@gmail.com>
2021-03-16 16:59:53 +0700

    create get cart item and add item to cart api

--- src/controller/cart.js
+++ src/controller/cart.js
@@ -1,5 +1,16 @@
 const Cart = require('../models/cart');
 
+
+function runUpdate(condition, updateData) {
+    return new Promise((resolve, reject) => {
+
+        Cart.findOneAndUpdate(condition, updateData, { upsert: true })
+            .then(result => resolve())
+            .catch(err => reject(err))
+    });
+}
+
+
 exports.addItemToCart = (req, res) => {
 
     Cart.findOne({ user: req.user._id })
@@ -7,35 +18,40 @@ exports.addItemToCart = (req, res) => {
         if(error) return res.status(400).json({ error });
         if(cart) {
             //Kiem tra neu gio hang da ton tai thi cap nhat so luong cua gio hang
+            let promiseArray = [];
 
-            const product = req.body.cartItems.product;
-            const item = cart.cartItems.find(c => c.product == product );
-            let condition, update;
-            if(item){
-                condition = { "user": req.user._id, "cartItems.product": product };
-                update = {
-                    "$set": {
-                        "cartItems.$": {
-                            ...req.body.cartItems,
-                            quantity: item.quantity + req.body.cartItems.quantity
+            req.body.cartItems.forEach((cartItem) => {
+                const product = cartItem.product;
+                const item = cart.cartItems.find(c => c.product == product );
+                let condition, update;
+                if(item){
+                    condition = { "user": req.user._id, "cartItems.product": product };
+                    update = {
+                        "$set": {
+                            "cartItems.$": cartItem
                         }
-                    }
-                };
-            }else{
-                condition = { user: req.user._id };
-                update = {
-                    "$push": {
-                        "cartItems": req.body.cartItems
-                    }
-                };
-            }
-            Cart.findOneAndUpdate(condition, update)
-            .exec((error, _cart) => {
-                if(error) return res.status(400).json({ error });
-                if(_cart){
-                    return res.status(201).json({ cart: _cart });
+                    };
+                }else{
+                    condition = { user: req.user._id };
+                    update = {
+                        "$push": {
+                            "cartItems": cartItem
+                        }
+                    };
                 }
+                promiseArray.push(runUpdate(condition, update))
+                
+                // Cart.findOneAndUpdate(condition, update)
+                // .exec((error, _cart) => {
+                //     if(error) return res.status(400).json({ error });
+                //     if(_cart){
+                //         return res.status(201).json({ cart: _cart });
+                //     }
+                // });
             });
+            Promise.all(promiseArray)
+            .then(response => res.status(201).json({ response }))
+            .catch(error => res.status(400).json({ error }))
         }else{
             //Neu gio hng chua ton tai thi tao moi gio hang
             const cart = new Cart({
@@ -50,5 +66,44 @@ exports.addItemToCart = (req, res) => {
                 }
             });
         }
-    })
+    });
 };
+
+// exports.addToCart = (req, res) => {
+//     const { cartItems } = req.body;
+//     if(cartItems){
+//         if(Object.keys(cartItems).length > 0){
+//             Cart.findOneAndUpdate({
+//                 "user": req.user._id
+//             }, {
+//                 "cartItems": cartItems
+//             }, {
+//                 upsert: true, new: true, setDefaultsOnInsert: true
+//             }, (error, cartItems) => {
+//                 if(error) return res.status(400).json({ error });
+//                 if(cartItems) res.status(201).json({ message: 'Added Successfully' });
+//             })
+//         }
+//     }
+// }
+
+exports.getCartItems = (req, res) => {
+    Cart.findOne({ user: req.user._id })
+    .populate('cartItems.product', '_id name price productPictures')
+    .exec((error, cart) => {
+        if(error) return res.status(400).json({ error });
+        if(cart){
+            let cartItems = {};
+            cart.cartItems.forEach((item, index) => {
+                cartItems[item.product._id.toString()] = {
+                    _id: item.product._id.toString(),
+                    name: item.product.name,
+                    img: item.product.productPictures[0].img,
+                    price: item.product.price,
+                    qty: item.quantity
+                }
+            });
+            res.status(200).json({ cartItems });
+        }
+    })
+}
--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const { addItemToCart } = require('../controller/cart');
+const { addItemToCart, getCartItems } = require('../controller/cart');
 const { userMiddleware, requireSignIn } = require('../middleware');
 const router = express.Router();
 
 router.post('/user/cart/addtocart', requireSignIn, userMiddleware, addItemToCart);
-
+router.post('/user/getCartItems', requireSignIn, userMiddleware, getCartItems );
 module.exports = router;
