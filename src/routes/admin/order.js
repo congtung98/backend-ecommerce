15febb5b456e2aa3249dc1d9c8aabb1bbef65d00
Hoan Vu Van <congtunggv@gmail.com>
2021-03-29 10:20:40 +0700

    create api get order detail

--- src/routes/admin/order.js
+++ src/routes/admin/order.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const { requireSignIn, adminMiddleware } = require("../../middleware");
-const { updateOrder } = require("../../controller/admin/order");
+const { updateOrder, getCustomerOrders } = require("../../controller/admin/order");
 const router = express.Router();
 
 router.post(`/order/update`, requireSignIn, adminMiddleware, updateOrder);
-
+router.post(
+    `/order/getCustomerOrders`,
+    requireSignIn,
+    adminMiddleware,
+    getCustomerOrders
+);
 module.exports = router;
